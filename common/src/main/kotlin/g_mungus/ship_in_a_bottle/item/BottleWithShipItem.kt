6834cdb01a0f57f947a362a7abe21c066d3f89df
James Green <jgreenop22@gmail.com>
2025-04-01 22:45:27 -0700

    tooltip and vlib datapack

--- common/src/main/kotlin/g_mungus/ship_in_a_bottle/item/BottleWithShipItem.kt
+++ common/src/main/kotlin/g_mungus/ship_in_a_bottle/item/BottleWithShipItem.kt
@@ -1,7 +1,22 @@
 package g_mungus.ship_in_a_bottle.item
 
+import net.minecraft.network.chat.Component
 import net.minecraft.world.item.BlockItem
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.TooltipFlag
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 
 class BottleWithShipItem(block: Block, properties: Properties): BlockItem(block, properties) {
+
+    override fun appendHoverText(
+        stack: ItemStack,
+        level: Level?,
+        tooltipComponents: MutableList<Component>,
+        isAdvanced: TooltipFlag
+    ) {
+        tooltipComponents.add(Component.literal("Ship: ${
+            stack.orCreateTag.getString("Ship")?: "Sloop"
+        }"))
+    }
 }
