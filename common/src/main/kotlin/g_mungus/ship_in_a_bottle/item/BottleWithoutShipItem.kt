8d0f6c240b5ac4b7a8c71ed57ddcd2a4db63763d
James Green <jgreenop22@gmail.com>
2025-04-01 22:04:59 -0700

    saving ships

--- common/src/main/kotlin/g_mungus/ship_in_a_bottle/item/BottleWithoutShipItem.kt
+++ common/src/main/kotlin/g_mungus/ship_in_a_bottle/item/BottleWithoutShipItem.kt
@@ -1,7 +1,75 @@
 package g_mungus.ship_in_a_bottle.item
 
+import g_mungus.ship_in_a_bottle.ShipInABottle
+import g_mungus.vlib.api.VLibGameUtils
+import net.minecraft.core.BlockPos
+import net.minecraft.core.particles.ParticleTypes
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.sounds.SoundEvents
+import net.minecraft.sounds.SoundSource
+import net.minecraft.world.InteractionResult
 import net.minecraft.world.item.BlockItem
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.block.Block
+import org.valkyrienskies.mod.common.getShipManagingPos
+import kotlin.random.Random
 
-class BottleWithoutShipItem(block: Block, properties: Properties): BlockItem(block, properties) {
+class BottleWithoutShipItem(block: Block, properties: Properties) : BlockItem(block, properties) {
+
+    override fun place(context: BlockPlaceContext): InteractionResult {
+        val level = context.level
+        val pos = context.clickedPos
+
+        val ship = level.getShipManagingPos(pos)
+
+
+        if (ship != null) {
+            if (level is ServerLevel) {
+                ship.shipAABB?.let {
+                    for (x in it.minX()..it.maxX()) {
+                        for (y in it.minY()..it.maxY()) {
+                            for (z in it.minZ()..it.maxZ()) {
+                                if (!level.getBlockState(BlockPos(x, y, z)).isAir && Random.nextDouble() > 0.7) {
+                                    level.sendParticles(
+                                        ParticleTypes.END_ROD,
+                                        x + 0.5,
+                                        y + 0.5,
+                                        z + 0.5,
+                                        1,
+                                        0.0,
+                                        0.0,
+                                        0.0,
+                                        0.0
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+
+                level.playSound(
+                    null,
+                    pos,
+                    SoundEvents.EVOKER_PREPARE_ATTACK,
+                    SoundSource.PLAYERS,
+                    1f,
+                    1f
+                )
+
+                VLibGameUtils.saveShipToTemplate(ShipInABottle.MOD_ID, level, ship.id, false, true)
+            }
+            context.player?.setItemInHand(
+                context.hand,
+                ItemStack(ShipInABottle.BOTTLE_WITH_SHIP_ITEM).apply {
+                    tag = CompoundTag().apply { putString("Ship", ship.slug.toString()) }
+                }
+            )
+            context.player?.swing(context.hand)
+            return InteractionResult.SUCCESS
+        } else {
+            return super.place(context)
+        }
+    }
 }
