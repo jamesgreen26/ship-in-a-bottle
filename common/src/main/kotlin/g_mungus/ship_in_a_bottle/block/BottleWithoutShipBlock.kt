3dfc189c526ed68e3f80ea9eb12f63b49da4f770
James Green <jgreenop22@gmail.com>
2025-04-02 20:49:08 -0700

    fix rendering

--- common/src/main/kotlin/g_mungus/ship_in_a_bottle/block/BottleWithoutShipBlock.kt
+++ common/src/main/kotlin/g_mungus/ship_in_a_bottle/block/BottleWithoutShipBlock.kt
@@ -4,13 +4,13 @@ import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
 import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.BlockGetter
-import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.Blocks
-import net.minecraft.world.level.block.RenderShape
-import net.minecraft.world.level.block.SimpleWaterloggedBlock
+import net.minecraft.world.level.LevelAccessor
+import net.minecraft.world.level.block.*
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.material.FluidState
+import net.minecraft.world.level.material.Fluids
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.VoxelShape
 
@@ -38,13 +38,35 @@ class BottleWithoutShipBlock(properties: Properties) : Block(properties), Simple
         return RenderShape.MODEL
     }
 
-    fun isTransparent(state: BlockState?, world: BlockGetter?, pos: BlockPos?): Boolean {
-        return true
-    }
-
     override fun getStateForPlacement(context: BlockPlaceContext): BlockState? {
         return super.getStateForPlacement(context)
             ?.setValue(BlockStateProperties.HORIZONTAL_FACING, context.horizontalDirection)
             ?.setValue(BlockStateProperties.WATERLOGGED, context.level.getBlockState(context.clickedPos).`is`(Blocks.WATER))
+    }
+
+    override fun getFluidState(state: BlockState): FluidState {
+        return if (state.getValue(BlockStateProperties.WATERLOGGED)) Fluids.WATER.getSource(false) else super.getFluidState(state)
+    }
+
+    override fun updateShape(
+        state: BlockState,
+        direction: Direction,
+        neighborState: BlockState,
+        world: LevelAccessor,
+        pos: BlockPos,
+        neighborPos: BlockPos
+    ): BlockState {
+        if (state.getValue(BlockStateProperties.WATERLOGGED)) {
+            world.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(world))
+        }
+        return super.updateShape(state, direction, neighborState, world, pos, neighborPos)
+    }
+
+    override fun rotate(state: BlockState, rotation: Rotation): BlockState {
+        return state.setValue(BlockStateProperties.HORIZONTAL_FACING, rotation.rotate(state.getValue(BlockStateProperties.HORIZONTAL_FACING)))
+    }
+
+    override fun mirror(state: BlockState, mirror: Mirror): BlockState {
+        return state.rotate(mirror.getRotation(state.getValue(BlockStateProperties.HORIZONTAL_FACING)))
     }
 }
--- common/src/main/kotlin/g_mungus/ship_in_a_bottle/client/ShipInABottleClient.kt
+++ common/src/main/kotlin/g_mungus/ship_in_a_bottle/client/ShipInABottleClient.kt
@@ -1,12 +1,7 @@
 package g_mungus.ship_in_a_bottle.client
 
 import g_mungus.ship_in_a_bottle.ShipInABottle
-import g_mungus.ship_in_a_bottle.ShipInABottle.BOTTLE_WITHOUT_SHIP_BLOCK
-import g_mungus.ship_in_a_bottle.ShipInABottle.BOTTLE_WITH_SHIP_BLOCK
 import g_mungus.ship_in_a_bottle.ShipInABottle.LOGGER
-import g_mungus.ship_in_a_bottle.ShipInABottle.WATER_DISPLAY_BLOCK
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
-import net.minecraft.client.renderer.RenderType
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers
 
 object ShipInABottleClient {
@@ -14,10 +9,6 @@ object ShipInABottleClient {
     @JvmStatic
     fun init() {
         LOGGER.info("client init")
-        BlockRenderLayerMap.INSTANCE.putBlock(BOTTLE_WITH_SHIP_BLOCK, RenderType.translucent())
-        BlockRenderLayerMap.INSTANCE.putBlock(BOTTLE_WITHOUT_SHIP_BLOCK, RenderType.translucent())
-        BlockRenderLayerMap.INSTANCE.putBlock(WATER_DISPLAY_BLOCK, RenderType.translucent())
-
         BlockEntityRenderers.register(ShipInABottle.BOTTLE_WITH_SHIP_BE_TYPE) { BottleWithShipBERenderer() }
     }
 }
